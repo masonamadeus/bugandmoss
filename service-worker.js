676212d514520e36dfc72bb6aabc1bb104a0f15d
Mason Amadeus <71096260+masonamadeus@users.noreply.github.com>
2025-08-19 19:23:11 -0400

    turns out he can patch ONE MORE time

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // A version number for our cache. Change this string to force an update.
-const CACHE_NAME = 'Bug-And-Moss-Soundboard-v022'; 
+const CACHE_NAME = 'Bug-And-Moss-Soundboard-v023'; 
 
 // The list of files that make up the "app shell".
 const urlsToCache = [
