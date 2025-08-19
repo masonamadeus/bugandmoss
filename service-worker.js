ccda8ce36c892e39e5fb39e10190708560aad4bd
Mason Amadeus <71096260+masonamadeus@users.noreply.github.com>
2025-08-19 18:43:43 -0400

    Tip your service workers

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // A version number for our cache. Change this string to force an update.
-const CACHE_NAME = 'Bug-And-Moss-Soundboard-v020'; 
+const CACHE_NAME = 'Bug-And-Moss-Soundboard-v021'; 
 
 // The list of files that make up the "app shell".
 const urlsToCache = [
