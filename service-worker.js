b3af9835b05f07203614bf379b81aa79346d36bf
Mason Amadeus <71096260+masonamadeus@users.noreply.github.com>
2025-08-20 13:52:08 -0400

    some updates to weather and soundboard

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // A version number for our cache. Change this string to force an update.
-const CACHE_NAME = 'Bug-And-Moss-Soundboard-v024'; 
+const CACHE_NAME = 'Bug-And-Moss-Soundboard-v025'; 
 
 // The list of files that make up the "app shell".
 const urlsToCache = [
